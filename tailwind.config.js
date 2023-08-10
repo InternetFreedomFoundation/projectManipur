4bdc0a557481992381455bbdae5e945bd73164d7
Ishan Velle <ishanvelle@no-reply.users.example.com>
2023-08-11 00:54:12 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans"],
+      },
+    },
+  },
+  plugins: [],
+};
